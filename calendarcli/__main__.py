ea566c7966d7e046be6f62e5832a6b33103559f4
Pandora <nevillanirut@gmail.com>
2021-09-08 07:39:07 +0700

    options

--- calendarcli/__main__.py
+++ calendarcli/__main__.py
@@ -1,9 +1,36 @@
+from os import path
 import sys
+
+
+
+unixOptions = "hau:d:l:"
+gunOptions =[
+    "help",
+    "add",
+    "date=",
+    "event=",
+    "update=",
+    "delete=",
+    "list",
+    "modify"
+]
+
 def main():
-    print('in main')
-    args = sys.argv[1:]
-    print('count of args :: {}'.format(len(args)))
-    for arg in args:
-        print('passed argument :: {}'.format(arg))
+    arg = sys.argv[1:]    
+    o = arg.pop(0)
+
+    if o in ("--help","-h"):
+        import calendarcli.help
+    elif o in ("--add","-a"):
+        print("add")
+    elif o in ("--update","-u"):
+        print("update")
+    elif o in ("--delete","-d"):
+        print("delete")
+    elif o in ("--list","-l"):
+        print("list")
+    else:import calendarcli.help
+
+
 if __name__ == '__main__':
     main()
