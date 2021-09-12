e3787d7fe042f4a89717496ad5f80437a742fc93
Pandora <nevillanirut@gmail.com>
2021-09-12 11:25:27 +0700

    delete file out of .gitignore

--- .old/Document.py
+++
@@ -1,6 +0,0 @@
-import DataManager
-
-def read_help_file():
-    import codecs
-    with codecs.open(DataManager.getPath('data/Document.txt'), encoding='utf-8') as help_file:
-       print(help_file.read())
